2fe33a5b1e4692755b16e7b06b2bd4e90285804f
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-08 14:36:08 +0530

    copy file from one dir to another using fs module

--- fs-module/index.js
+++ fs-module/index.js
@@ -1,4 +1,5 @@
 let fs = require('fs');
+let path = require('path');
 
 // create file using fs module of node js
 // fs.writeFileSync('f1.txt','file created using fs module of node js');
@@ -8,3 +9,15 @@ let fs = require('fs');
 
 // Update or append data in file
 // fs.appendFileSync('f1.txt',' this is appended data in the file');
+
+
+// copy file (f2.txt) from sourceDir to destinationDir
+
+let sourcePath = 'D:/Practice/Node JS/fs-module/sourceDir/f2.txt';
+let destinationDirPath = 'D:/Practice/Node JS/fs-module/destinationDir';
+let fileName = path.basename(sourcePath);
+
+let destinationPath = path.join(destinationDirPath,fileName);
+fs.copyFileSync(sourcePath,destinationPath);
+
+
