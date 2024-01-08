5936b43e5ac1c377078dde192e7892b8a0f79087
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-08 13:16:06 +0530

    write, delete and update text in file using fs module done

--- fs-module/index.js
+++ fs-module/index.js
@@ -1,4 +1,10 @@
 let fs = require('fs');
 
 // create file using fs module of node js
-fs.writeFileSync('f1.txt','file created using fs module of node js');
+// fs.writeFileSync('f1.txt','file created using fs module of node js');
+
+// delete file using fs module
+// fs.unlinkSync('f1.txt');
+
+// Update or append data in file
+// fs.appendFileSync('f1.txt',' this is appended data in the file');
